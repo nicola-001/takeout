219e727f411e4d935a4b01c3f53f4b4723798edd
nicola <2629137145@qq.com>
2025-09-11 21:07:01 +0800

    查看购物车业务功能完成

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCardController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCardController.java
@@ -1,16 +1,18 @@
 package com.sky.controller.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.sky.context.BaseContext;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/shoppingCart")
@@ -29,5 +31,18 @@ public class ShoppingCardController {
         log.info("添加购物车,{}", shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
+    }
+
+    /*
+     * 查询购物车
+     * */
+    @GetMapping("list")
+    @ApiOperation("查询购物车")
+    public Result<List<ShoppingCart>> list() {
+        //获取用户id
+        Long userId = BaseContext.getCurrentId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, userId);
+        return Result.success(shoppingCartService.list(queryWrapper));
     }
 }
